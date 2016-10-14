2a582a70b75e4a971991782a52eb05f9f8099ab8
Dave van Herten <dvanherten@outlook.com>
2016-10-13 22:12:29 -0400

    Adding a simple library for a nuget package

---
+++ ExampleNugetPackage/src/ExampleNugetPackage/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleNugetPackage
+{
+    public class HelloWorld
+    {
+        public static void SayIt()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
